f85e8d771f71e8d6755e88cfaa7b76bbd360993f
KruTatiana <ilm63.rus@gmail.com>
2024-04-06 18:54:31 +0300

    styles commit

--- list-of-tariffs/src/App.js
+++ list-of-tariffs/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import themeBlue from './component/theme-blue.module.css'
+import themeGreen from './component/theme-green.module.css'
+import themeBlack from './component/theme-black.module.css'
+import themeRed from './component/theme-red.module.css'
+import TariffCard from './component/TariffCard/TariffCard';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="cards_container">
+        <TariffCard
+          price = {300}
+          speed = "10"
+          theme = {themeBlue}
+        />
+        <TariffCard
+          price = {450}
+          speed = "50"
+          theme = {themeGreen}
+        />
+        <TariffCard
+          price = {550}
+          speed = "100"
+          theme = {themeRed}
+        />
+        <TariffCard
+          price = {1000}
+          speed = "200"
+          theme = {themeBlack}
+        />
+      </div>
     </div>
   );
 }
